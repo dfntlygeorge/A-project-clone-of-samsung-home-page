97579739435eca0da4cd39362afba2658a16ef24
dfntlygeorge <g.a.donayre.business@gmail.com>
2024-06-13 11:08:01 +0800

    Changes: 1. Full screen search popout. 2. Hamburger menu. -sidebar -search bar -dropside shits -inner dropdown shits 3. Fixed the overlay issues. 4. Added a lot of utility class.
    Notes for the next commit:
    
    1. Orgranize the CSS and HTML, napakagulo.
    2. Pwede na mag start sa hero section.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Wait for the DOM to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function () {
+  // General Variables
   // -----Overlay effect for dropdown menu-----
   const dropdownMenus = document.querySelectorAll(".dropdown-menu");
 
@@ -14,6 +15,82 @@ document.addEventListener("DOMContentLoaded", function () {
 
     dropdown.addEventListener("mouseleave", () => {
       navOverlay.classList.remove("active");
+    });
+  });
+
+  // FEATURE: SEARCH POPOUT
+
+  // Selecting multiple search buttons
+  const searchBtns = document.querySelectorAll("#nav-search-btn");
+  const searchOverlay = document.querySelector(".search-overlay");
+  const searchContainer = document.querySelector(".search-container");
+  const closeBtn = document.querySelector(".close-btn");
+
+  // Add click event listener to each search button
+  searchBtns.forEach((searchBtn) => {
+    searchBtn.addEventListener("click", () => {
+      searchOverlay.classList.add("active");
+      searchContainer.classList.add("active");
+    });
+  });
+  // Add click event listener to close button
+  closeBtn.addEventListener("click", () => {
+    searchOverlay.classList.remove("active");
+    searchContainer.classList.remove("active");
+  });
+
+  // FEATURE: HAMBURGER MENU
+
+  const hamMenu = document.querySelector(".ham-menu");
+  const sideMenu = document.querySelector(".side-menu");
+  const menuOverlay = document.querySelector(".menu-overlay");
+  const sdCloseBtn = document.querySelector(".side-close-btn");
+
+  hamMenu.addEventListener("click", () => {
+    sideMenu.classList.add("active");
+    menuOverlay.classList.add("active");
+  });
+  sdCloseBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+    menuOverlay.classList.remove("active");
+  });
+
+  const sidedownMenus = document.querySelectorAll(".sidedown-menu");
+  const shiftingMenu = document.querySelector(".shifting");
+  const backBtn = document.querySelector(".back-btn");
+
+  sidedownMenus.forEach((sidedownMenu) => {
+    sidedownMenu.addEventListener("click", () => {
+      sidedownMenu.classList.add("active");
+      backBtn.classList.add("active");
+      shiftingMenu.style.overflowY = "hidden";
+    });
+  });
+  backBtn.addEventListener("click", () => {
+    sidedownMenus.forEach((sidedownMenu) => {
+      sidedownMenu.classList.remove("active");
+      backBtn.classList.remove("active");
+      shiftingMenu.style.overflowY = "scroll";
+    });
+  });
+
+  // Dropside dapat kase
+  const sdMenus = document.querySelectorAll(".sd-menu");
+
+  sdMenus.forEach((sdMenu) => {
+    sdMenu.addEventListener("click", () => {
+      // Check if the clicked FAQ is already active
+      const isActive = sdMenu.classList.contains("active");
+
+      // Remove active class from all FAQs
+      sdMenus.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      // If the clicked FAQ was not active, activate it
+      if (!isActive) {
+        sdMenu.classList.add("active");
+      }
     });
   });
 
